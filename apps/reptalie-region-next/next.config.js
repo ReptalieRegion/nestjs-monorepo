680206947fd7259ed432489d64a4e929d834e26e
yunchanpark <yongp99@nate.com>
2023-05-11 21:33:50 +0900

    transpilePackages 변경

--- apps/reptalie-region-next/next.config.js
+++ apps/reptalie-region-next/next.config.js
@@ -1,10 +1,11 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withTM = require('next-transpile-modules')(['@reptalie-region/ui']);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    transpilePackages: ['@reptalie-region/ui'],
+    experimental: {
+        appDir: true,
+    },
 };
 
-module.exports = withTM(nextConfig);
+module.exports = nextConfig;

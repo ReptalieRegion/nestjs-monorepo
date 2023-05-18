952f95b53eb24ad6977333adbfbec27919344141
yunchanpark <yongp99@nate.com>
2023-05-18 23:57:31 +0900

    delete: 일상공유 페이지에 test로 넣어논 reactQuery 삭제

--- apps/reptalie-region-next/src/app/home/share/page.tsx
+++ apps/reptalie-region-next/src/app/home/share/page.tsx
@@ -1,12 +1,4 @@
-import getQueryClient from '@/contexts/react-query/getQueryClient';
-
-async function test() {
-    const res = await fetch('http://localhost:3333/api/users/nickname-exists?nickname=윤댕');
-    return res.json();
-}
 const Share = async () => {
-    const queryClient = getQueryClient();
-    await queryClient.prefetchInfiniteQuery(['h'], test);
     return <div>share</div>;
 };
 

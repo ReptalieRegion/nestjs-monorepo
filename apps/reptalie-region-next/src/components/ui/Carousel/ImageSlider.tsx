a1e44eb01d80789e5ddac2b8525c60b3923c08d6
yunchanpark <yongp99@nate.com>
2023-06-02 14:44:25 +0900

    fix: 이미지 스와이프 커스텀 훅으로 적용

--- apps/reptalie-region-next/src/components/ui/Carousel/ImageSlider.tsx
+++ apps/reptalie-region-next/src/components/ui/Carousel/ImageSlider.tsx
@@ -1,10 +1,9 @@
 'use client';
 
 import Image from 'next/image';
-import { TouchEventHandler, useState } from 'react';
 import { useRef } from 'react';
 import test from '@/assets/images/contemplative-reptile.jpg';
-import { newLongTouch } from '@/utils/gestures/LongTouch';
+import useSwiper from '@/hooks/useSwiper';
 
 const images = [
     { src: test, alt: '1' },
@@ -13,105 +12,13 @@ const images = [
 ];
 
 const ImageSlider = () => {
-    const scrollViewRef = useRef<HTMLDivElement>(null);
     const imagesRef = useRef<null[] | HTMLDivElement[]>([]);
-    const currentIndexRef = useRef<number>(0);
-    const [touchPosition, setTouchPosition] = useState<number | null>(null);
-
-    const handleTouchStart: TouchEventHandler<HTMLDivElement> = (event) => {
-        newLongTouch.onStart();
-        const touchDown = event.touches[event.touches.length - 1].clientX;
-        setTouchPosition(touchDown);
-    };
-
-    const handleTouchEnd: TouchEventHandler<HTMLDivElement> = (event) => {
-        if (!scrollViewRef.current || !touchPosition) {
-            return;
-        }
-        newLongTouch.onEnd();
-        const isLongTab = newLongTouch.getIsLongTab();
-        const currentTouch = event.changedTouches[0].clientX;
-        const currentIndex = currentIndexRef.current;
-        const isMoveRight = touchPosition - currentTouch > 0 && currentIndex < images.length - 1;
-
-        const isMoveLeft = touchPosition - currentTouch < 0 && currentIndex > 0;
-        const scrollViewElement = scrollViewRef.current;
-        const contentWidth = scrollViewElement.clientWidth;
-
-        if (!isLongTab) {
-            if (isMoveRight) {
-                const movePosition = contentWidth * (currentIndex + 1);
-                scrollViewRef.current.scrollTo({
-                    left: movePosition,
-                    behavior: 'smooth',
-                });
-                currentIndexRef.current += 1;
-            }
-
-            if (isMoveLeft) {
-                const movePosition = contentWidth * (currentIndex - 1);
-                scrollViewRef.current.scrollTo({
-                    left: movePosition,
-                    behavior: 'smooth',
-                });
-                currentIndexRef.current -= 1;
-            }
-        } else {
-            const currentImageRef = imagesRef.current[currentIndex];
-            if (!currentImageRef) {
-                return;
-            }
-            const contentPositionX = scrollViewElement.getBoundingClientRect().x;
-
-            if (isMoveRight) {
-                const nextImageRef = imagesRef.current[currentIndex + 1];
-                if (!nextImageRef) {
-                    return;
-                }
-
-                const nextImagePositionX = nextImageRef.getBoundingClientRect().x;
-
-                const isMoveNextImage = contentWidth / 2 > nextImagePositionX - contentPositionX;
-                const movePosition = isMoveNextImage ? contentWidth * (currentIndex + 1) : contentWidth * currentIndex;
-
-                scrollViewRef.current.scrollTo({
-                    left: movePosition,
-                    behavior: 'smooth',
-                });
-
-                currentIndexRef.current += isMoveNextImage ? 1 : 0;
-            }
-
-            if (isMoveLeft) {
-                const prevImageRef = imagesRef.current[currentIndex - 1];
-                if (!prevImageRef) {
-                    return;
-                }
-
-                const prevImagePositionX = prevImageRef.getBoundingClientRect().right;
-                const isMovePrevImage = contentWidth / 2 < prevImagePositionX - contentPositionX;
-                const movePosition = isMovePrevImage ? contentWidth * (currentIndex - 1) : contentWidth * currentIndex;
-
-                scrollViewRef.current.scrollTo({
-                    left: movePosition,
-                    behavior: 'smooth',
-                });
-                currentIndexRef.current -= isMovePrevImage ? 1 : 0;
-            }
-        }
-
-        setTouchPosition(null);
-    };
+    const swiperRef = useSwiper<HTMLDivElement>(images.length);
 
     return (
         <div className="relative flex flex-row justify-center mb-10pxr">
-            <div
-                ref={scrollViewRef}
-                className="h-[250px] overflow-y-hidden overflow-x-scroll rounded-md"
-                onTouchStart={handleTouchStart}
-                onTouchEnd={handleTouchEnd}
-            >
-                <div className={`w-[calc(300vw-120px)]`}>
+            <div ref={swiperRef} className="h-[250px] overflow-y-hidden overflow-x-hidden rounded-md scrollbar-hide">
+                <div className={`w-[calc(300vw-120px)] h-[calc(300vw-120px)]`}>
                     {images.map(({ src, alt }, index) => {
                         return (
                             <div
--- apps/reptalie-region-next/src/hooks/useSwiper.ts
+++ apps/reptalie-region-next/src/hooks/useSwiper.ts
@@ -1,36 +1,128 @@
+import customDirection, { TMoveType } from '@/utils/gestures/Direction';
+import customlongTouch from '@/utils/gestures/LongTouch';
+import customScroll from '@/utils/gestures/Scroll';
 import { useEffect, useRef } from 'react';
 
-export const useSwiper = () => {
-    const swiperRef = useRef<HTMLDivElement>(null);
-
-    const onTouchStart = (e: TouchEvent) => {
-        console.log(e);
-    };
-
-    const onTouchMove = (e: TouchEvent) => {
-        console.log(e);
-    };
-
-    const onTouchEnd = (e: TouchEvent) => {
-        console.log(e);
-    };
+const useSwiper = <T extends HTMLElement>(maxSlideCount = 0) => {
+    const swiperRef = useRef<T>(null);
+    const slideCountRef = useRef(0);
 
     useEffect(() => {
-        const swiperNode = swiperRef.current;
-        if (!swiperNode) {
+        const swiperElement = swiperRef.current;
+        if (!swiperElement) {
             return;
         }
 
-        swiperNode.addEventListener('touchstart', onTouchStart);
-        swiperNode.addEventListener('touchmove', onTouchMove);
-        swiperNode.addEventListener('touchend', onTouchEnd);
+        const { clientWidth, clientHeight } = swiperElement;
+        const longTouch = customlongTouch();
+        const direction = customDirection()({
+            width: clientWidth,
+            height: clientHeight,
+        });
+        const scroll = customScroll({ element: swiperRef.current })();
+
+        const touchStart = (event: TouchEvent) => {
+            longTouch.start();
+            direction.start(event);
+            scroll.start(event);
+        };
+
+        const touchMove = (event: TouchEvent) => {
+            direction.move(event);
+            const moveType = direction.getMoveType();
+            const isHorizontality = moveType === 2 || moveType === 3;
+            if (isHorizontality) {
+                event.preventDefault();
+                event.stopPropagation();
+                scroll.moveHorizontality(event);
+            }
+        };
+
+        const touchEnd = () => {
+            const isLongTouch = longTouch.getIsLongTouch();
+            const moveType = direction.getMoveType();
+            direction.end();
+            longTouch.end();
+
+            if (isLongTouch) {
+                longTouchMove(moveType);
+                return;
+            }
+
+            shortTouchMove(moveType);
+        };
+
+        const shortTouchMove = (moveType: TMoveType) => {
+            const leftDirection = moveType === 2;
+            if (leftDirection) {
+                moveNext();
+                return;
+            }
+
+            const rightDirection = moveType === 3;
+            if (rightDirection) {
+                movePrev();
+                return;
+            }
+        };
+
+        const longTouchMove = (moveType: TMoveType) => {
+            const sliderWidth = swiperElement.clientWidth;
+            const scrollLeft = swiperElement.scrollLeft;
+            const slideCount = slideCountRef.current;
+            const leftDirection = scrollLeft > sliderWidth * slideCount + sliderWidth / 2 && moveType === 2;
+            if (leftDirection) {
+                moveNext();
+                return;
+            }
+
+            const rightDirection = scrollLeft < sliderWidth * slideCount - sliderWidth / 2 && moveType === 3;
+            if (rightDirection) {
+                movePrev();
+                return;
+            }
+
+            moveCurrent();
+        };
+
+        const moveCurrent = () => {
+            const slideCount = slideCountRef.current;
+            const movePosition = slideCount * swiperElement.clientWidth;
+            scroll.scrollToLeftSmooth(movePosition);
+        };
+
+        const movePrev = () => {
+            const slideCount = slideCountRef.current;
+            if (slideCount > 0) {
+                const movePosition = (slideCount - 1) * swiperElement.clientWidth;
+                scroll.scrollToLeftSmooth(movePosition);
+                slideCountRef.current -= 1;
+            }
+        };
+
+        const moveNext = () => {
+            const slideCount = slideCountRef.current;
+            if (slideCount < maxSlideCount - 1) {
+                const movePosition = (slideCount + 1) * swiperElement.clientWidth;
+                scroll.scrollToLeftSmooth(movePosition);
+                slideCountRef.current += 1;
+            }
+        };
+
+        swiperElement.addEventListener('touchstart', touchStart);
+        swiperElement.addEventListener('touchmove', touchMove);
+        swiperElement.addEventListener('touchend', touchEnd);
+        swiperElement.addEventListener('touchcancel', touchEnd);
 
         return () => {
-            swiperNode.removeEventListener('touchstart', onTouchStart);
-            swiperNode.removeEventListener('touchmove', onTouchMove);
-            swiperNode.removeEventListener('touchend', onTouchEnd);
+            swiperElement.removeEventListener('touchstart', touchStart);
+            swiperElement.removeEventListener('touchmove', touchMove);
+            swiperElement.removeEventListener('touchend', touchEnd);
+            swiperElement.removeEventListener('touchcancel', touchEnd);
         };
-    }, []);
+    }, [maxSlideCount]);
 
     return swiperRef;
 };
+
+export default useSwiper;

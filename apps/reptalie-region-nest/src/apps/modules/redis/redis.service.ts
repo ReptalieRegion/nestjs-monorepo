8b039ec93b2b420b7d7099c5d07db81067fa4d1a
yunchanpark <yongp99@nate.com>
2023-06-03 16:12:17 +0900

    fix: jwt payload 잘못된 부분 임시 수정 및 redis type guard 설정

--- apps/reptalie-region-nest/src/apps/modules/redis/redis.service.ts
+++ apps/reptalie-region-nest/src/apps/modules/redis/redis.service.ts
@@ -7,16 +7,22 @@ export class RedisService {
     constructor(@Inject(CACHE_MANAGER) private readonly redisStore: RedisStore) {}
 
     async set(key: string, data: string, ttl?: number) {
-        await this.redisStore.set(key, data, ttl);
+        if (typeof key === 'string') {
+            await this.redisStore.set(key, data, ttl);
+        }
     }
 
     async get(key: string) {
-        const result = await this.redisStore.get(key);
-        return result;
+        if (typeof key === 'string') {
+            const result = await this.redisStore.get(key);
+            return result;
+        }
     }
 
     async del(key: string) {
-        await this.redisStore.del(key);
+        if (typeof key === 'string') {
+            await this.redisStore.del(key);
+        }
     }
 
     async mset(args: Array<[string, unknown]>, ttl?: number | undefined) {

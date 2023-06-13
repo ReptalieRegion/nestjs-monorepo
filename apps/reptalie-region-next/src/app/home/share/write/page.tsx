4467e9e67c57e3f0a0692124ad6232f4a7044d3f
yunchanpark <yongp99@nate.com>
2023-06-13 19:59:29 +0900

    add: 일상공유 작성하기 페이지 생성

---
+++ apps/reptalie-region-next/src/app/home/share/write/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import ScrollComponentContext from '@/contexts/scroll/ScrollContext';
+
+export default function WritePage() {
+    return (
+        <ScrollComponentContext>
+            <div>글쓰기</div>
+        </ScrollComponentContext>
+    );
+}
--- apps/reptalie-region-next/src/components/home/share/atoms/PostWrite.tsx
+++ apps/reptalie-region-next/src/components/home/share/atoms/PostWrite.tsx
@@ -1,14 +1,18 @@
 'use client';
 
 import PostWriteIcon from '@/assets/icons/post_write.svg';
+import { useRouter } from 'next/navigation';
 
 interface IPostWriteProps {
     className?: string;
 }
 
 const PostWrite = ({ className }: IPostWriteProps) => {
+    const router = useRouter();
+
     return (
         <div
+            onClick={() => router.push('share/write')}
             className={`absolute bottom-0pxr w-50pxr h-50pxr bg-teal-150 rounded-full flex justify-center items-center active:scale-[0.85] transition-all ${className}`}
         >
             <PostWriteIcon />

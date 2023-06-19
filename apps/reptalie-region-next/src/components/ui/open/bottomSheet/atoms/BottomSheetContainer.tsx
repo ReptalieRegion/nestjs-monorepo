7b65538ff7919897fcbda23e2303f5d95a3674a0
yunchanpark <yongp99@nate.com>
2023-06-19 21:56:07 +0900

    fix: react-native에서 safe-area가 추가되어 마진 생김으로 인한 backdrop 보이는 현상 해결

--- apps/reptalie-region-next/src/components/ui/open/bottomSheet/atoms/BottomSheetContainer.tsx
+++ apps/reptalie-region-next/src/components/ui/open/bottomSheet/atoms/BottomSheetContainer.tsx
@@ -8,7 +8,7 @@ interface IBottomSheetContainerProps {
 
 const BottomSheetContainer = ({ children, height, enableHeightTransition }: IBottomSheetContainerProps) => {
     return (
-        <div onClick={(event) => event.stopPropagation()} className="absolute left-0pxr bottom-0pxr w-full overflow-y-scroll">
+        <div onClick={(event) => event.stopPropagation()} className="absolute left-0pxr bottom-[-1px] w-full overflow-y-scroll">
             <div
                 style={{ transition: enableHeightTransition ? 'height 0.3s ease-out' : undefined, height }}
                 className="w-full bg-white opacity-100 rounded-t-2xl pl-20pxr pr-20pxr pb-15pxr h-full"

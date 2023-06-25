91a90573c8c37c463073cace47e5fb622f7fe53e
yunchanpark <yongp99@nate.com>
2023-06-25 23:18:03 +0900

    add: webview-bridge를 사용해서 react-native route 제어

--- apps/reptalie-region-next/src/components/home/share/atoms/PostWrite.tsx
+++ apps/reptalie-region-next/src/components/home/share/atoms/PostWrite.tsx
@@ -1,18 +1,20 @@
 'use client';
 
 import PostWriteIcon from '@/assets/icons/post_write.svg';
-import { useRouter } from 'next/navigation';
+import { Navigate } from '@/utils/webveiw-bridge/Navigate';
 
 interface IPostWriteProps {
     className?: string;
 }
 
 const PostWrite = ({ className }: IPostWriteProps) => {
-    const router = useRouter();
+    const handleRouteImageCrop = () => {
+        Navigate.push({ route: 'ImageCropPage' });
+    };
 
     return (
         <div
-            onClick={() => router.push('share/write')}
+            onClick={handleRouteImageCrop}
             className={`absolute bottom-0pxr w-50pxr h-50pxr bg-teal-150 rounded-full flex justify-center items-center active:scale-[0.85] transition-all ${className}`}
         >
             <PostWriteIcon />

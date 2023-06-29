bc035540a52fcd5ac1b7a7dcd584b836894a70f4
yunchanpark <yongp99@nate.com>
2023-06-29 17:43:47 +0900

    fix: header 컴포넌트 변경

--- apps/reptalie-region-next/src/components/ui/layouts/Header.tsx
+++ apps/reptalie-region-next/src/components/ui/layouts/Header.tsx
@@ -1,19 +1,38 @@
 'use client';
 
-import { ReactNode } from 'react';
 import Logo from '@/assets/icons/logo.svg';
+import BackButton from '@/assets/icons/back_button.svg';
+import { IHeaderProps, TLeftIcon } from '<Header>';
+import { useRouter } from 'next/navigation';
 
-interface IHeaderProps {
-    center?: ReactNode;
-    right?: ReactNode;
-}
+type TLeftIconMap = {
+    [key in TLeftIcon]: {
+        Icon: any;
+        onClick: () => void;
+    };
+};
 
-const Header = ({ center, right }: IHeaderProps) => {
+const Header = ({ left, center, right }: IHeaderProps) => {
+    const router = useRouter();
+    const leftIconMap: TLeftIconMap = {
+        back: { Icon: BackButton, onClick: () => router.back() },
+        cancel: { Icon: BackButton, onClick: () => router.back() },
+        logo: { Icon: Logo, onClick: () => router.replace('/home') },
+    };
+    const leftInfo = left && leftIconMap[left];
+
     return (
-        <div className="left-0pxr top-0pxr w-full bg-white flex flex-row justify-between items-center pt-10pxr pb-10pxr pl-20pxr pr-20pxr border-b-[1px] shadow-md">
-            <div className="flex flex-col items-center justify-center">
-                <Logo />
-            </div>
+        <div className="left-0pxr top-0pxr w-full bg-white flex flex-row justify-between items-center p-10pxr border-b-[1px]">
+            {leftInfo !== undefined && (
+                <div
+                    className={`flex flex-col items-center justify-center ${
+                        left === 'back' ? 'active:opacity-50 active:scale-90 active:translate-x-[-7px] active:duration-100' : ''
+                    }`}
+                    onClick={leftInfo.onClick}
+                >
+                    <leftInfo.Icon />
+                </div>
+            )}
             <div>{center}</div>
             <div className="w-32pxr">{right}</div>
         </div>

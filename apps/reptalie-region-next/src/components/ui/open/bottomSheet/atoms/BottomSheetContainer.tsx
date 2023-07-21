c630dd6080e073ce49dbd78439cad69d04043236
yunchanpark <yongp99@nate.com>
2023-07-21 19:21:02 +0900

    fix: BottomSheet Component 변경

--- apps/reptalie-region-next/src/components/ui/open/bottomSheet/atoms/BottomSheetContainer.tsx
+++ apps/reptalie-region-next/src/components/ui/open/bottomSheet/atoms/BottomSheetContainer.tsx
@@ -20,7 +20,7 @@ const BottomSheetContainer = ({ children, height, enableHeightTransition, close 
         <div onClick={(event) => event.stopPropagation()} className="absolute left-0pxr bottom-[-1px] w-full overflow-y-scroll">
             <div
                 style={{ transition: enableHeightTransition ? 'height 0.3s ease-out' : undefined, height }}
-                className="w-full bg-white opacity-100 rounded-t-2xl"
+                className="w-full bg-white opacity-100 rounded-t-2xl flex flex-col"
             >
                 {children}
             </div>
--- apps/reptalie-region-next/src/components/ui/open/bottomSheet/atoms/BottomSheetHeader.tsx
+++ apps/reptalie-region-next/src/components/ui/open/bottomSheet/atoms/BottomSheetHeader.tsx
@@ -1,9 +1,12 @@
-import { ForwardedRef, forwardRef } from 'react';
+import { ForwardedRef, PropsWithChildren, forwardRef } from 'react';
 
-const BottomSheetHeader = (_: unknown, ref: ForwardedRef<HTMLDivElement>) => {
+const BottomSheetHeader = ({ children }: PropsWithChildren, ref: ForwardedRef<HTMLDivElement>) => {
     return (
-        <div ref={ref} className="flex justify-center pt-15pxr pb-15pxr">
-            <div className="w-[30%] h-5pxr bg-gray-400 rounded-md" />
+        <div ref={ref}>
+            <div className="flex justify-center pt-15pxr pb-15pxr">
+                <div className="w-[30%] h-5pxr bg-gray-400 rounded-md" />
+            </div>
+            {children}
         </div>
     );
 };

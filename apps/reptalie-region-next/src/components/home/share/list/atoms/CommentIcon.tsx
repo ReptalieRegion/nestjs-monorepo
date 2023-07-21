723bea96fcca4526d347b713206c06b68c9a7f05
yunchanpark <yongp99@nate.com>
2023-07-21 19:27:11 +0900

    delete: CommentIcon 필요없는 코드 삭제

--- apps/reptalie-region-next/src/components/home/share/list/atoms/CommentIcon.tsx
+++ apps/reptalie-region-next/src/components/home/share/list/atoms/CommentIcon.tsx
@@ -2,20 +2,12 @@
 
 import { IPostsData } from '<API>';
 import Comment from '@/assets/icons/comment.svg';
-import useUIPromptManager from '@/hooks/useUIPromptManager';
-import PostCommentBottomSheet, { IPostCommentBottomSheetProps } from '../ui-prompts/bottomSheet/PostCommentBottomSheet';
 
 type ICommentProps = Pick<IPostsData, 'postId'>;
 
 const CommentIcon = ({ postId }: ICommentProps) => {
-    const { openPrompt } = useUIPromptManager();
-
     const handleClickComment = () => {
-        openPrompt<IPostCommentBottomSheetProps>({
-            Component: PostCommentBottomSheet,
-            promptType: 'bottomSheet',
-            props: {},
-        });
+        console.log(postId);
     };
 
     return (

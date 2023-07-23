f1918837fbcbc5fc27c0a500ea940e7b53ecaf0b
yunchanpark <yongp99@nate.com>
2023-07-23 15:10:21 +0900

    fix: webview-bridge 1.0.25 NextJSNavigation 타입 수정

--- packages/webview-bridge/src/next-js/navigation/types.ts
+++ packages/webview-bridge/src/next-js/navigation/types.ts
@@ -2,6 +2,16 @@ import { BridgeFunction, NotExistsArguments } from '../../common';
 
 interface NavigateOptions {}
 
+enum PrefetchKind {
+    AUTO = 'auto',
+    FULL = 'full',
+    TEMPORARY = 'temporary',
+}
+
+interface PrefetchOptions {
+    kind: PrefetchKind;
+}
+
 export type TNextJSNavigationPush = {
     href: string;
     options?: NavigateOptions;
@@ -9,10 +19,12 @@ export type TNextJSNavigationPush = {
 
 export type TNextJSNavigationReplace = {
     href: string;
+    options?: NavigateOptions;
 };
 
 export type TNextJSNAvigationPrefetch = {
     url: string;
+    options?: PrefetchOptions;
 };
 
 export interface INextJSNavigation {

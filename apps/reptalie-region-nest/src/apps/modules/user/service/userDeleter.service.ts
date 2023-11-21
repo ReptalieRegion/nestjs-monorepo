5d0e7fc194669ca8cf0b0ed8faa53ac053b47319
yunchanpark <yongp99@nate.com>
2023-11-21 18:23:10 +0900

    add: fcmToken 삭제 로직 추가

--- apps/reptalie-region-nest/src/apps/modules/user/service/userDeleter.service.ts
+++ apps/reptalie-region-nest/src/apps/modules/user/service/userDeleter.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { FollowRepository } from '../repository/follow.repository';
 import { UserRepository } from '../repository/user.repository';
 import { UserSearcherService, UserSearcherServiceToken } from './userSearcher.service';
@@ -14,4 +14,14 @@ export class UserDeleterService {
         @Inject(UserSearcherServiceToken)
         private readonly userSearcherService: UserSearcherService,
     ) {}
+
+    async fcmTokenDelete(userId: string) {
+        const result = await this.userRepository.updateOne({ _id: userId }, { $set: { fcmToken: '' } }).exec();
+
+        if (result.modifiedCount === 0) {
+            throw new InternalServerErrorException('Failed to delete user fcmToken.');
+        }
+
+        return;
+    }
 }

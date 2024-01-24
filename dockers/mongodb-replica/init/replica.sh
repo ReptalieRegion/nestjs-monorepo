7ae72d359afaacd1f15761d7d79ccdf9935fa463
yunchanpark <yongp99@nate.com>
2024-01-24 15:17:51 +0900

    fix: mongoDB local 레플리카셋 포트 변경 및 적용

--- dockers/mongodb-replica/init/replica.sh
+++ dockers/mongodb-replica/init/replica.sh
@@ -1,12 +1,12 @@
 # replica.sh
-dockerize -wait tcp://mongodb1:27017 -wait tcp://mongodb2:27017 -wait tcp://mongodb3:27017
+dockerize -wait tcp://mongodb1:27017 -wait tcp://mongodb2:27018 -wait tcp://mongodb3:27019
 
 mongosh --host mongodb1 --port 27017 --username admin --password 1234 --eval "rs.initiate({
     _id: \"myRepl\",
     members: [
-        {_id: 0, host: \"mongodb1\"},
-        {_id: 1, host: \"mongodb2\"},
-        {_id: 2, host: \"mongodb3\"}
+        {_id: 0, host: \"mongodb1:27017\"},
+        {_id: 1, host: \"mongodb2:27018\"},
+        {_id: 2, host: \"mongodb3:27019\"}
     ]
 })"
 mongosh --host mongodb1 --port 27017 --username admin --password 1234 --eval "rs.status()"

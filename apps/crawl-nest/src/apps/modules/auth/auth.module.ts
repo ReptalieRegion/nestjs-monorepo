3ce66c657b890303013a488fff0827a36cb7829a
yunchanpark <yongp99@nate.com>
2024-01-24 15:24:03 +0900

    add: 신규 가입 슬랙 메시지 기록 로직 추가

--- apps/crawl-nest/src/apps/modules/auth/auth.module.ts
+++ apps/crawl-nest/src/apps/modules/auth/auth.module.ts
@@ -24,6 +24,7 @@ import { TempUserRepository } from './repository/tempUser.repository';
         CustomJwtModule,
         PassportModule,
         ImageModule,
+        NotificationModule,
         forwardRef(() => UserModule),
         forwardRef(() => ShareModule),
         forwardRef(() => DiaryModule),
